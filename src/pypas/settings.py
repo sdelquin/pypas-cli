29ec51e969e3c346b1be7bdf8474cd4193d71ad9
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-06-14 10:15:45 +0100

    Migrate upload to put

--- src/pypas/settings.py
+++ src/pypas/settings.py
@@ -6,7 +6,7 @@ from prettyconf import config
 PYPAS_BASE_URL = config('PYPAS_BASE_URL', default='https://pypas.es/')
 PYPAS_AUTH_URLPATH = urljoin(PYPAS_BASE_URL, '/access/auth/')
 PYPAS_GET_EXERCISE_URLPATH = urljoin(PYPAS_BASE_URL, '/exercises/{exercise_slug}/get/')
-PYPAS_UPLOAD_ASSIGNMENT_URLPATH = urljoin(PYPAS_BASE_URL, '/assignments/{exercise_slug}/upload/')
+PYPAS_PUT_ASSIGNMENT_URLPATH = urljoin(PYPAS_BASE_URL, '/assignments/{exercise_slug}/put/')
 PYPAS_STATS_URLPATH = urljoin(PYPAS_BASE_URL, '/assignments/stats/')
 
 EXERCISE_CONFIG_FILE = config('EXERCISE_CONFIG_FILE', default='.pypas.toml')
